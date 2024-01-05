b242bccfe61bc38b560f2fabbbb31a984cecdba7
Robert O. Alabi <wolo22001@gmail.com>
2024-01-05 20:14:40 +0000

    add navigation to the the project

---
+++ src/components/PageNotFount.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PageNotFount() {
+  return (
+    <>
+      <PageNav />
+      <div>The page you are requesting is not available!!!</div>
+    </>
+  );
+}
+
+export default PageNotFount;
