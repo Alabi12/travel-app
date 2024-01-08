70f7111fa3d97612eae89cf76819297af22bcf6a
Robert O. Alabi <wolo22001@gmail.com>
2024-01-08 12:28:53 +0000

    add searchParams hook

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,6 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 function MapContainer() {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const latitude = searchParams.get('lat');
+  const longitude = searchParams.get('long');
+  console.log(latitude);
+  console.log(longitude);
   return (
-    <div>MapContainer</div>
+    <div>
+      lat: {latitude}
+      long: {longitude}
+
+      <button onClick={() => setSearchParams({
+        lat: 1, long: 2
+      })}>Change</button>
+    </div>
   )
 }
 
--- src/pages/CityDetails.jsx
+++ src/pages/CityDetails.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom"
+import { useParams, useSearchParams } from "react-router-dom"
 
 function CityDetails() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const latitude = searchParams.get('lat');
+  const longitude = searchParams.get('long');
   const {cityId} = useParams();
 
   return (
-    <div>City Details: {cityId}</div>
+    <div>City Details: {cityId}
+      <div>lat: {latitude}</div>
+      <div>long: {longitude}</div>
+    </div>
   )
 }
 
